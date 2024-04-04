8d2a753c032086518d69c62f93d7c8b235d49bc0
Matheus Oliveira <matheus0mco@gmail.com>
2024-04-04 08:48:31 -0300

    feat:created select script

--- QueriesApp/Program.cs
+++ QueriesApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int qty = 10;
-int[] accountsRange = { 0, 10 };
-double[] valuesRange = { -100, 100 };
+int[] accountsRange = { 0, qty };
+double[] valuesRange = { qty * qty * -1, qty * qty };
 long[] timestampRange = { 0, new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds() };
 Random random = new Random();
 var pocos = new List<POCO>();
@@ -9,7 +9,6 @@ for (int i = 0; i < qty; i++)
 {
     pocos.Add(new POCO
     {
-        // TODO: add accountId to hash to use in selects
         AccountId = random.Next(accountsRange[0], accountsRange[1]),
         Value = double.Round(random.NextDouble() * valuesRange[random.Next(0, 2)], 2),
         Timestamp = random.NextInt64(timestampRange[0], timestampRange[1])
@@ -18,11 +17,18 @@ for (int i = 0; i < qty; i++)
 
 Console.WriteLine("Finished creating inserts");
 
-// TODO: create selects
-/*
-*   use randomized accounts from hashset
-*   randomize ranges (30, 45, 60, 90)
-*/
+List<POCO> repositoryMock = new List<POCO>();
+int[] dateRanges = { 30, 45, 60, 90 };
+List<IEnumerable<POCO>> selects = new List<IEnumerable<POCO>>();
+for (int i = 0; i < qty; i++)
+{
+    var range = dateRanges[random.Next(0, dateRanges.Count())];
+    var accountId = random.Next(accountsRange[0], accountsRange[1]);
+    var timestamp = new DateTimeOffset(DateTime.Now.AddDays(range * -1)).ToUnixTimeSeconds();
+    selects.Add(repositoryMock.Where(_ => _.AccountId == accountId && _.Timestamp > timestamp));
+}
+
+Console.WriteLine("Finished creating selects");
 
 class POCO
 {
