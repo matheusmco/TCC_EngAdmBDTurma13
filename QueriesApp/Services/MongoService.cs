ce7a96844a4cf68da861ddbdb0f51de6a14400ae
Matheus Oliveira <matheus0mco@gmail.com>
2024-08-19 09:08:25 -0300

    fix:update mongo

--- QueriesApp/Services/MongoService.cs
+++ QueriesApp/Services/MongoService.cs
@@ -26,11 +26,14 @@ public class MongoService : ServiceBase<MongoPOCO>
             var document = db.Find(_ => _.AccountId == i.AccountId).FirstOrDefault();
             if (document == null)
                 newDocuments.Add(i);
-            else // TODO: add in a list, to update after
-                UpdatePoco(i, document);
+            else
+                updates.Add(UpdatePoco(i, document));
         }
 
         InsertNewPocos(newDocuments);
+        
+        foreach(var u in updates)
+            db.UpdateOne(u.Item1, u.Item2);
 
         Console.WriteLine($"{TimeSpan.FromTicks(DateTime.Now.Ticks - beginTime).TotalSeconds}");
     }
@@ -40,13 +43,13 @@ public class MongoService : ServiceBase<MongoPOCO>
                group i by i.AccountId into g
                select new MongoPOCO { AccountId = g.Key, Statement = g.SelectMany(_ => _.Statement).ToList() };
 
-    private void UpdatePoco(MongoPOCO i, MongoPOCO document)
+    private (FilterDefinition<MongoPOCO>, UpdateDefinition<MongoPOCO>) UpdatePoco(MongoPOCO i, MongoPOCO document)
     {
         var statement = document.Statement;
         statement.AddRange(i.Statement);
         var filter = Builders<MongoPOCO>.Filter.Eq(_ => _.AccountId, i.AccountId);
         var update = Builders<MongoPOCO>.Update.Set(_ => _.Statement, statement);
-        db.UpdateMany(filter, update);
+        return (filter, update);
     }
 
     private void InsertNewPocos(List<MongoPOCO> newDocuments)
