18018e5c52e7fa2d8af7e5cff05b006ff410a90a
Matheus Oliveira <matheus0mco@gmail.com>
2024-08-20 08:22:39 -0300

    added results

--- execution-script.ps1
+++ execution-script.ps1
@@ -1,14 +1,14 @@
-# for($i = 0; $i -lt 100; $i++)
-# {
-#     dotnet run --project .\QueriesApp\QueriesApp.csproj select 10000 mongo >> result_10k_select_mongo.txt
-# }
+for($i = 0; $i -lt 10; $i++)
+{
+    dotnet run --project .\QueriesApp\QueriesApp.csproj select 1 mongo >> result_1_select_mongo.txt
+}
 
-# for($i = 0; $i -lt 100; $i++)
-# {
-#     dotnet run --project .\QueriesApp\QueriesApp.csproj select 100000 mongo >> result_100k_select_mongo.txt
-# }
+for($i = 0; $i -lt 10; $i++)
+{
+    dotnet run --project .\QueriesApp\QueriesApp.csproj select 10 mongo >> result_10_select_mongo.txt
+}
 
-# for($i = 0; $i -lt 100; $i++)
-# {
-#     dotnet run --project .\QueriesApp\QueriesApp.csproj select 1000000 mongo >> result_1M_select_mongo.txt
-# }
+for($i = 0; $i -lt 10; $i++)
+{
+    dotnet run --project .\QueriesApp\QueriesApp.csproj select 100 mongo >> result_100_select_mongo.txt
+}
