3c89303a0c51a31333279ee602dcbd87b8e9e82d
Matheus Oliveira <matheus0mco@gmail.com>
2024-08-20 19:29:01 -0300

    feat:added mongo as document and as event

--- execution-script.ps1
+++ execution-script.ps1
@@ -1,14 +1,34 @@
 for($i = 0; $i -lt 10; $i++)
 {
-    dotnet run --project .\QueriesApp\QueriesApp.csproj select 1 mongo >> result_1_select_mongo.txt
+    docker-compose -f .\docker-compose-mongo.yml up -d
+    dotnet run --project .\QueriesApp\QueriesApp.csproj insert 10000 mongo_event >> result_10K_insert_mongo_event.txt
+    docker-compose -f .\docker-compose-mongo.yml down
 }
 
 for($i = 0; $i -lt 10; $i++)
 {
-    dotnet run --project .\QueriesApp\QueriesApp.csproj select 10 mongo >> result_10_select_mongo.txt
+    docker-compose -f .\docker-compose-mongo.yml up -d
+    dotnet run --project .\QueriesApp\QueriesApp.csproj insert 100000 mongo_event >> result_100K_insert_mongo_event.txt
+    docker-compose -f .\docker-compose-mongo.yml down
 }
 
 for($i = 0; $i -lt 10; $i++)
 {
-    dotnet run --project .\QueriesApp\QueriesApp.csproj select 100 mongo >> result_100_select_mongo.txt
+    docker-compose -f .\docker-compose-mongo.yml up -d
+    dotnet run --project .\QueriesApp\QueriesApp.csproj insert 1000000 mongo_event >> result_1M_insert_mongo_event.txt
+}
+
+for($i = 0; $i -lt 10; $i++)
+{
+    dotnet run --project .\QueriesApp\QueriesApp.csproj select 1 mongo_event >> result_100_select_mongo_event.txt
+}
+
+for($i = 0; $i -lt 10; $i++)
+{
+    dotnet run --project .\QueriesApp\QueriesApp.csproj select 10 mongo_event >> result_100_select_mongo_event.txt
+}
+
+for($i = 0; $i -lt 10; $i++)
+{
+    dotnet run --project .\QueriesApp\QueriesApp.csproj select 100 mongo_event >> result_100_select_mongo_event.txt
 }
